8d4f1c50b174b4545617ee74ca98d09a3eab2488
TienN <tienn@tmtco.asia>
2022-05-30 14:55:21 +0700

    Add Product Entity to DB

---
+++ src/Acme.BookStore.Domain/Products/Product.cs
@@ -0,0 +1,23 @@
+﻿using Acme.BookStore.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Products
+{
+    public class Product : AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public ProductType Type { get; set; }
+        public int Stock { set; get; }
+        public int ViewCount { set; get; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
